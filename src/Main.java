88cd25734fe5742a7e587209ee9018b5307beff1
Kato-Ismail <Katoismail9999@gmail.com>
2024-09-09 06:04:12 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+   static int a = 23;//static variable
+    static int b = 42;//static variable
+
+    public  static void main(String[]args)
+    {
+        System.out.println("a"+" "+a);
+        System.out.println("b"+" "+b);
+    }
+}
